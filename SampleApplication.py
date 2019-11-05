30634d4533ae7f81ea6c0b750ca7ea2d99d00250
Mike Ligthart <mike.ligthart@gmail.com>
2019-11-05 10:59:07 +0100

    adds skeleton code to run hello world on robot

---
+++ SampleApplication.py
@@ -0,0 +1,16 @@
+import AbstractApplication as Base
+
+
+class SampleApplication(Base.AbstractApplication):
+    def main(self):
+        self.setLanguage('en-US')
+        self.sayAnimated('Hello, world!')
+
+    def onRobotEvent(self, event):
+        print event
+
+
+# Run the application
+sample = SampleApplication()
+sample.main()
+sample.stop()
